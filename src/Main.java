a422837196b8b75dfd49783caf94a706a0a5e877
Allan Felipe Malinoski <allanfelipedk@gmail.com>
2022-02-08 08:43:57 -0300

    Variables

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+
+public class Main {
+	public static void main(String[] args)
+	{
+		/*               Primeiro program
+		System.out.println("Primeiro programa em java!");
+		*/
+		
+		//-------------------------------------------
+		
+		//                Variaveis
+		int idade = 18;
+		System.out.println("Minha idade � -> " + idade);
+		idade = 19;
+		System.out.println("Minha idade � -> " + idade);
+		//Tipo de Variaveis
+		int myNumber = 10;
+		String myString = "Allan";
+		boolean myBool = true;
+		char myChar = 'A';
+		double myDouble = 35.3;
+		System.out.println(myNumber);
+		System.out.println(myString);
+		System.out.println(myBool);
+		System.out.println(myChar);
+		System.out.println(myDouble);
+		
+		//-------------------------------------------
+		
+		//                Constantes
+	}
+}
