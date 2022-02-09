0cd37c073a1fe7b8dfd7e04e2373f8dde697a8cd
Allan Felipe Malinoski <allanfelipedk@gmail.com>
2022-02-09 10:19:25 -0300

    switch - case

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.SystemMenuBar;
 
 public class Main {
 	public static void main(String[] args)
@@ -8,11 +9,11 @@ public class Main {
 		
 		//-------------------------------------------
 		
-		//                Variaveis
+		/*                Variaveis
 		int idade = 18;
-		System.out.println("Minha idade � -> " + idade);
+		System.out.println("Minha idade -> " + idade);
 		idade = 19;
-		System.out.println("Minha idade � -> " + idade);
+		System.out.println("Minha idade -> " + idade);
 		//Tipo de Variaveis
 		int myNumber = 10;
 		String myString = "Allan";
@@ -24,9 +25,89 @@ public class Main {
 		System.out.println(myBool);
 		System.out.println(myChar);
 		System.out.println(myDouble);
-		
+		*/
+
 		//-------------------------------------------
 		
-		//                Constantes
+		/* 	            Constantes
+		final int vidaMaxima = 10;
+		//vidaMaxima = 11 -> nao posso mudar o valor pq ela é constante 
+		System.out.println(vidaMaxima);
+		*/
+
+		//-------------------------------------------
+
+		/*         		Arrays
+		String[] nome = new String[5];
+		nome[0] = "Allan";
+		nome[1] = "Felipe";
+		System.out.println(nome[0]);
+		//-> Array multidimensional
+		String[][] nomes = new String[5][5];
+		nomes[0][0] = "Mathues";
+		nomes[1][1] = "Allan";
+		nomes[2][2] = "Felipe";
+		System.out.println(nomes[0][0]);
+		*/
+
+		//-------------------------------------------
+
+		//				Condicoes
+		//<, >, ==, !=
+		/*
+		if 
+		else if
+		else
+		*/
+		/*
+		int vida = 101;
+		if(vida > 100)//<, >, ==, !=
+		{
+			System.out.println("A vida é maior que 100");
+		}
+		else
+		{
+			System.out.println("A vida nao é maior que 100");
+		}
+		String nome = "Allan";
+		if(nome == "Allan")
+		{
+			System.out.println("O nome é igual ah " + nome);
+		}
+		else if(nome == "joao")
+		{
+			System.out.println("O nome é igual ah " + nome);
+		}
+		else
+		{
+			System.out.println("Nenhum nome é igual!");
+		}
+		boolean var = true;
+		if(var == true)
+		{
+			System.out.println("Verdade");
+		}
+		else
+		{
+			System.out.println("Falso");
+		}
+		*/
+		
+		//-------------------------------------------
+
+		// 			Switch - Case
+		int vida = 100;
+		switch(vida)
+		{
+			case 90:
+			System.out.println("Minha vida é 90");
+				break;
+			case 30:
+			System.out.println("Minha vida é 30");
+				break;
+			default:
+			System.out.println("Nenhum condicao bateu!");
+				break;
+		}
 	}
 }
