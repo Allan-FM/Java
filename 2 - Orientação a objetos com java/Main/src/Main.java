af63f34a65e8bc922f332482181d7acebd4faa68
Allan <allanfelipedk@gmail.com>
2022-03-30 12:48:50 -0300

    Interfaces

--- 2 - Orientação a objetos com java/Main/src/Main.java
+++ 2 - Orientação a objetos com java/Main/src/Main.java
@@ -1,29 +1,27 @@
-public class Main extends TesteAbstract
+public class Main implements PlayerInterface
 {
-    public static void main(String[] args) 
+
+    public static void main(String[] args)
     {
-        // // Player player = new Player();
-        // // player.playerStart();
-        // // Enemy enemy = new Enemy();
-        // // enemy.enemyStart();
-        // Player player2 = new Player();
-        // player2.playerLife(0);
-    }
-    public void chamarMetodoAbstrato()
-    {
-        IniciarJogo();
+    	
     }
 
-    public void instaciateMain2()
-    {
-        new Main2().println();
-    }
-    private class Main2
-    {
-        public void println()
-        {
-           // System.out.println("Chamando metodo!");
-        }
-    }
-   
+
+	@Override
+	public void vida() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void morrer() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void perderVida() {
+		// TODO Auto-generated method stub
+		
+	}
 }
---
+++ 2 - Orientação a objetos com java/Main/src/PlayerInterface.java
@@ -0,0 +1,7 @@
+public interface PlayerInterface
+{
+	public void vida();
+	public void morrer();
+	public void perderVida();
+	
+}
